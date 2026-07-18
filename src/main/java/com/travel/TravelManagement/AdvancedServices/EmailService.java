45f8365401743f03d6125e1ecdb0fff8054222e3
Saisree Andavarapu <saisreeandavarapu@gmail.com>
2026-07-18 11:36:40 +0530

    add gmail config

--- src/main/java/com/travel/TravelManagement/AdvancedServices/EmailService.java
+++ src/main/java/com/travel/TravelManagement/AdvancedServices/EmailService.java
@@ -12,12 +12,37 @@ public class EmailService {
     private JavaMailSender mailSender;
 
 
-    public void sendEmail(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        mailSender.send(message);
+    public void sendEmail(
+            String email,
+            String subject,
+            String body){
+
+        try{
+
+            System.out.println(
+                    "Before Sending Mail");
+
+            SimpleMailMessage message =
+                    new SimpleMailMessage();
+
+            message.setTo(email);
+            message.setSubject(subject);
+            message.setText(body);
+
+            mailSender.send(message);
+
+            System.out.println(
+                    "Mail Sent Successfully");
+
+        }
+        catch(Exception e){
+
+            System.out.println(
+                    "Mail Sending Failed");
+
+            e.printStackTrace();
+
+        }
 
     }
 
